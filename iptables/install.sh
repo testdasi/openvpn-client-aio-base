423b3d835ef1b10bf19d1e9abcefeb2121c0b82b
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-10 20:45:16 +0100

    Create install.sh

---
+++ iptables/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# install more packages
+apt-get -y update \
+    && apt-get -y install dnsutils wget sipcalc
+
+# install openvpn
+apt-get install -y openvpn
+apt-get install -y iptables
+
+# install stubby and clean config
+apt-get -y install stubby \
+    && mkdir -p /etc/stubby \
+    && rm -rf /etc/stubby/*
+
+# install dante server
+apt-get -y install dante-server \
+    && rm -f /etc/danted.conf
+
+# install tinyproxy
+apt-get -y install tinyproxy \
+    && mkdir -p /etc/tinyproxy \
+    && rm -rf /etc/tinyproxy/*
+
+# install torsocks
+apt-get -y install torsocks \
+    && mkdir -p /etc/tor \
+    && rm -rf /etc/tor/*
+
+# install privoxy
+apt-get -y install privoxy \
+    && mkdir -p /etc/privoxy \
+    && rm -rf /etc/privoxy/*
+
+# Clean up
+apt-get -y autoremove \
+    && apt-get -y autoclean \
+    && apt-get -y clean \
+    && rm -fr /tmp/* /var/tmp/* /var/lib/apt/lists/*

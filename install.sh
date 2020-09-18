6dac0d5ebffe1aaf68e71f6e370b07d126d4a0ee
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-18 20:44:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # install more packages
 apt-get -y update \
-    && apt-get -y install dnsutils wget sipcalc
+    && apt-get -y install dnsutils wget sipcalc curl
 
 # install openvpn
 apt-get install -y openvpn
